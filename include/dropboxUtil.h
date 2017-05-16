6f97b1d83a8a265c63ebd805fd1aa4b3e80a255b
Adriano Benin <adriano.benin@gmail.com>
2017-05-16 20:13:26 -0300

    use proper type for data size

--- include/dropboxUtil.h
+++ include/dropboxUtil.h
@@ -5,7 +5,11 @@
 #define SEND_NAME "SEND_NAME"
 #define EO_LIST "*END_OF_LIST_TAG*"
 
+#include <stdint.h>
+
 typedef enum { false, true } bool;
+
+typedef uint32_t datasize_t;
 
 char *read_line(void);
 char **split_args(char *command);
